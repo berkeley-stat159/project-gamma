a01e5fb5f1b46bd83aae0e01d4a892eee078d742
Xinyue Zhou <xinyue233@berkeley.edu>
2015-11-22 11:21:18 -0800

    roi_extraction

--- code/tmp_xinyue/roi_extraction.py
+++ code/tmp_xinyue/roi_extraction.py
@@ -4,27 +4,6 @@ import nibabel as nib
 import itertools as itt
 import matplotlib.pyplot as plt
 
-dic = {}
-with open ("/Users/Xinyue_star/Desktop/Final_Proj/project-gamma/code/tmp_xinyue/net_roi.txt") as f:
-	for lines in f:
-		(key_region, k_roi, val1, val2, val3)= lines.split()
-		loc = [int(val1),int(val2),int(val3)]
-		if key_region not in dic:
-			dic[key_region] = {}
-		dic[key_region][k_roi] = loc
-
-
-
-in_brain_mask = np.mean(fmri_data, axis=-1) > 5000
-ss = np.mean(fmri_data, axis=-1)
-sss = np.reshape(ss,(91*109*91,))
-plt.hist(sss,bins=50)
-plt.show()
-
-
-
-
-mm_to_vox = npl.inv(img.affine)
 def co2vox(coordinate,mm_to_vox):
 	#I find this will give me some decimal number, which is unreasonable
 	#indices
@@ -45,14 +24,27 @@ def ROI_region(fmri):
 Input: fmri data index of the center of RIO region
 return: corresponding frm indices in that region
 '''
-def filter_ROI(vox_indc):
+def filter_ROI(vox_indc, in_brain_mask):
 	return [v for v in vox_indc if in_brain_mask[v]]
 	# tmp = np.array([in_brain_mask[ind] for ind in vox_indc])
 	# return(np.array(vox_indc)[tmp])
 
+dic = {}
+with open ("/Users/Xinyue_star/Desktop/Final_Proj/project-gamma/code/tmp_xinyue/net_roi.txt") as f:
+	for lines in f:
+		(key_region, k_roi, val1, val2, val3)= lines.split()
+		loc = [int(val1),int(val2),int(val3)]
+		if key_region not in dic:
+			dic[key_region] = {}
+		dic[key_region][k_roi] = loc
+
+in_brain_mask = np.mean(fmri_data, axis=-1) > 5000
+
+mm_to_vox = npl.inv(img.affine)
+
 coor = dic['Default']['LIP']
 b = ROI_region(co2vox(coor,mm_to_vox))
-a = filter_ROI(b)
+a = filter_ROI(b, in_brain_mask)
 
 
 
