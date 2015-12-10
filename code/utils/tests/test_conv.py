684550ed19b5ff88b62548e27c4ed21c161de979
Xinyue Zhou <xinyue233@berkeley.edu>
2015-12-09 20:55:49 -0800

    test_std

--- code/utils/tests/test_conv.py
+++ code/utils/tests/test_conv.py
@@ -21,3 +21,21 @@ def test_conv_target_non_target():
   assert_almost_equal(actual_error_convolved, expected_error_convolved)
   assert_almost_equal(actual_target_convolved, expected_target_convolved)
   assert_almost_equal(actual_nontarget_convolved, expected_nontarget_convolved)
+
+def test_std():
+  task_fname = os.path.join(os.path.dirname(__file__), "test_data", "test_cond003.txt")
+  actual_target_std = conv.conv_std(10, task_fname, 1)
+  expected_target_std = np.array([ 0., -0.00030452, -0.00358456, -0.01000677, -0.01545791,-0.01714284, -0.01517165, -0.01111583, -0.00658934, -0.00265908])
+  assert_almost_equal(actual_target_std, expected_target_std )
+  
+
+# def test_main():
+#   task_fname = os.path.join(os.path.dirname(__file__), "test_data", "test_cond003.txt")
+#   error_fname = os.path.join(os.path.dirname(__file__), "test_data", "test_cond007.txt")
+#   actual_target_convolved, actual_nontarget_convolved, actual_error_convolved = conv.conv_target_non_target(10, task_fname, error_fname, 1, tr_divs = 10)
+#   expected_target_convolved = np.array([0., 0.00106485, 0.01253479, 0.03499249, 0.05405446, 0.05994645, 0.05305345, 0.03887074, 0.02304214, 0.00929849])
+#   expected_nontarget_convolved = np.array([ 0., 0.00069492, 0.01071944, 0.03264745, 0.05266825, 0.06, 0.05417416, 0.04044389, 0.02457532, 0.01051738])
+#   expected_error_convolved = np.array([0.] * 10)
+#   assert_almost_equal(actual_error_convolved, expected_error_convolved)
+#   assert_almost_equal(actual_target_convolved, expected_target_convolved)
+#   assert_almost_equal(actual_nontarget_convolved, expected_nontarget_convolved)
